1a0fbb568e39d1771a6ac18efc590029b88e9130
Anthony <athonly@gmail.com>
2026-02-11 22:07:35 -0800

    hf

--- tests/test_parallel_worker_failures.py
+++ tests/test_parallel_worker_failures.py
@@ -147,9 +147,7 @@ async def test_parallel_worker_continue_on_failure(test_file_setup_and_cleanup, 
         data
     )
 
-    # Send each worker half of their chunks
-    for key in list(data.keys())[0]:
-        mock_response.set_range_end_n_send(key, segment_size)
+    mock_response.set_range_end_n_send(str(segment_size - 1), segment_size)
 
     task_id = dm.add_download(mock_url, mock_file_name)
     async_thread_runner.submit(dm.start_download(task_id, use_parallel_download=True))
